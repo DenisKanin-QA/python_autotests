329ae2491374e3230c3e3779fd1f28702cf57864
Denis Kanin <126958168+DenisKanin-QA@users.noreply.github.com>
2024-01-16 20:55:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import requests
 
-token = "96af7be4eefc73f971d81c4fc80d11fb"
+token = "_________________"
 
-'''response_create= requests.post('https://api.pokemonbattle.me:9104/pokemons', json={
+response_create= requests.post('https://api.pokemonbattle.me:9104/pokemons', json={
                 "name":"Tama",
                 "photo":"https://dolnikov.ru/pokemons/albums/001.png"},
                 headers={"Content-Type":"application/json",
                          "trainer_token": token,
             })
 
-print(response_create.text)'''
+print(response_create.text)
 
-'''response_rename= requests.put('https://api.pokemonbattle.me:9104/pokemons', json={
+response_rename= requests.put('https://api.pokemonbattle.me:9104/pokemons', json={
                 "pokemon_id": "27613",
                 "name": "Nuts",
                 "photo": "https://dolnikov.ru/pokemons/albums/008.png"},
                 headers={"Content-Type":"application/json",
                          "trainer_token": token,
             })
-print(response_rename.text)'''
+print(response_rename.text)
 
 response_add_pokeball= requests.post('https://api.pokemonbattle.me:9104/trainers/add_pokeball', json={
                 "pokemon_id": "27613"},
@@ -26,32 +26,4 @@ response_add_pokeball= requests.post('https://api.pokemonbattle.me:9104/trainers
                          "trainer_token": token,
             })
 
-print(response_add_pokeball.text)
-
-
-
-
-
-'''response = requests.post('https://api.pokemonbattle.me:9104/trainers/reg', json={
-    "trainer_token": token,
-    "email": "S781@yandex.ru",
-    "password": "Den123"}, headers = {"Content-Type":"application/json"})
-
-print(response.text)'''
-
-'''response_activation = requests.post('https://api.pokemonbattle.me:9104/trainers/confirm_email',
-                                    json={"trainer_token": token}, 
-                                    headers = {"Content-Type":"application/json"})
-
-
-
-print(response_activation.status_code)'''
-
-
-
-
-'''if response_activation.status_code == 200:
-    print('OK')
-else:
-    print('not ok')'''
-    
+print(response_add_pokeball.text)  
